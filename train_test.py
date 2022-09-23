85b30efd034128b6c7a3dd0bb4adba9d65d0d6af
tlaw009 <sxl961@miami.edu>
2022-09-23 15:37:21 -0400

    train in progress

--- train_test.py
+++ train_test.py
@@ -18,12 +18,12 @@ from IPython import display
 
 from stargan_v2.m_starganv2 import StarGAN2
 
-# Performs hyperparam and import checks
 # relative
 dataset = "afhq"
 
 train_dataset_path=os.path.dirname(os.path.abspath(__file__))+"/datasets/"+dataset+"/train"
 weight_path = os.path.dirname(os.path.abspath(__file__))+"/weights"
+
 gen_staging_params = ((64, 128, 256), (3, 3, 3),
                  (2,2,0), (2,2,0))
 
